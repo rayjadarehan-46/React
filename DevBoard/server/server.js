b3ee40da8e9c536d8cef86e62385adb930252f22
rehan <rehanrayjada46@gmail.com>
2026-09-14 22:31:20 +0530

    Prepare DevBoard for Deployment

--- DevBoard/server/server.js
+++ DevBoard/server/server.js
@@ -10,7 +10,13 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
-app.use(cors())
+app.use(
+    cors({
+        origin:
+            process.env.CLIENT_URL ||
+            "http://localhost:5173"
+    })
+)
 app.use(express.json())
 app.use("/api/auth" , authRoutes)
 app.use("/api/projects" , projectRoutes)
@@ -34,6 +40,6 @@ app.post("/api/test", (req, res) => {
 
 connectDB()
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`)
 })
--- DevBoard/src/api/projectsApi.js
+++ DevBoard/src/api/projectsApi.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api/projects"
+const API_URL = `${import.meta.env.VITE_API_URL}/api/projects`
 
 export const getProjects = async (token) => {
     const response = await fetch(API_URL, {
